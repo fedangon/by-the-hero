6e45e496bf7ed4f1884a82d7770914394db708be
Felipe Daniel <emaildofelipedaniel@gmail.com>
2020-03-29 11:58:51 -0300

    Mobile da aplicação e ajustes no back-end

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,11 +1,11 @@
 const connection = require('../database/connection');
 
 module.exports = {
-
-    async index (request, response) {
-        
+    async index (request, response) {        
         const { page = 1 } = request.query;
         
+        const [count] = await connection('incidents').count();
+
         const incidents = await connection('incidents')
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
@@ -19,7 +19,7 @@ module.exports = {
                 'ongs.uf'
             ]);
             
-        response.header('X-TOTAL-COUNT', count['count(*)']);
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
